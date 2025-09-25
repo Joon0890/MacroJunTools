e9b268eeffffbfc0abfd229baa6025195aeb43c9
joon0890 <kimjunhee2483@gmail.com>
2025-09-25 14:37:51 +0900

    수정

--- config/__init__.py
+++
@@ -1,7 +0,0 @@
-from .config_manager import interactive_config
-from .config_manager import interactive_env
-from .config_manager import load_config
-from .config_manager import load_env
-from .config_manager import load_settings
-from .config_manager import save_config
-from .config_manager import save_env
--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import argparse
-from config import interactive_env
 from core import RunEverytimeAutoLike
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Main Entry Point")
-    subparsers = parser.add_subparsers(dest="command", help="Sub-command to run")
-
-    # Setting
-    setting_parser = subparsers.add_parser("setting", help="Setting config and .env file")
-
+    subparsers = parser.add_subparsers(dest="command", help="Sub-command to run")\
+    
     # Everytime
     everytime_parser = subparsers.add_parser("everytime", help="Run Everytime auto-like")
     everytime_parser.add_argument("--headless", action="store_true", help="Run in headless mode")
@@ -16,9 +12,7 @@ if __name__ == "__main__":
     # 실행
     args = parser.parse_args()
 
-    if args.command == "setting":
-        interactive_env()
-    elif args.command == "everytime":
+    if args.command == "everytime":
         RunEverytimeAutoLike(headless=args.headless)
     else:
         parser.print_help()
